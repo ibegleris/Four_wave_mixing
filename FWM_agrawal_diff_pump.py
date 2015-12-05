2ecf37e9d8856a6a4604550a01153127adf8c213
Ioannis Begleris <i.begleris@soton.ac.uk>
2015-12-05 14:29:51 +0000

    the multimode FWM code works, need to clean it up and run it now

---
+++ FWM_agrawal_diff_pump.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 16:35:27 2015
+
+@author: Ioannis Begleris
+"""
+
+from __future__ import division
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.constants import pi,c
+from scipy.integrate import  ode
+from scipy.io import loadmat
+from scipy.integrate import dblquad
+import sys
+from peakdetect import peakdetect
+plt.ioff()
+
+def dbm_to_w(dBm):
+    """This function converts a power given in dBm to a power given in W."""
+    return 1e-3*10**((dBm)/10.)
+
+
+def w_to_dbm(W):
+    """This function converts a power given in W to a power given in dBm."""
+    return 10.*np.log10(W)+30
+
+
+def xpm_spm(i,A):
+    "calculates the XPM-SPM term in the FWM equations(Agrawal)"
+    sums = 0
+    for k in range(4):
+        if k != i:
+            sums +=overlap1[i,k]*np.abs(A[k])**2
+    sums *=2
+    sums +=overlap1[i,i]*np.abs(A[i])**2
+    return sums*A[i]
+
+
+
+def integrand(i, A, zz,omega,Dk,a,n2):
+        i_vec = np.array([[1,2,3],[0,2,3],[3,0,1],[2,0,1]])
+        ii = i_vec[i,:]
+        temp = 2*overlap2[i] * np.conj(A[ii[0]]) * A[ii[1]] * A[ii[2]]
+        temp +=xpm_spm(i,A)
+        gama = n2*omega/c
+        return 1j*gama *temp
+
+
+def system(zz,AB,omega,Dk,a,n2):
+    try:
+        temp = AB[0]
+    except:
+        temp = AB
+        AB = np.copy(zz)
+        zz = np.copy(temp)
+        del temp
+        pass
+    dABdz=np.zeros(4,dtype='complex')
+    A0 = AB[0] + AB[1]*1j
+    A1 = AB[2] + AB[3]*1j
+    A2 = AB[4] + AB[5]*1j
+    A3 = AB[6] + AB[7]*1j
+    A = np.array([A0, A1, A2,A3])
+
+    for i in range(4):
+        dABdz[i] = integrand(i,A,zz,omega[i],Dk,a,n2)
+
+    return dABdz.view(np.float64)
+
+
+def integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,a,n2):
+    r = ode(system).set_integrator(int_method)
+    r.f_params =(omega,Dk,a,n2,)
+    r.set_initial_value(AB0.view(np.float64),np.float64(zmin))
+    count = 1
+    num_steps = (zmax - zmin)/dz +1
+    while count < num_steps and r.successful():
+            r.integrate(r.t+dz)
+            count+=1
+    if r.successful():
+        AB_final[:,i,j] = r.y.view(np.complex128)
+    return AB_final[:,i,j], r.successful()
+
+
+def field0(y,x):
+    w = w_vec[0]
+    return np.exp(-(x**2+y**2)/w**2)
+
+
+def field1(y,x):
+    w = w_vec[1]
+    return (2*2**0.5*x/w)*np.exp(-(x**2+y**2)/w**2)
+
+
+def over1(i,j,B,B_func):
+    if i == j:
+        if  B[i] == 'lp01':
+            return 1/161
+        else:
+            return 1/170
+    r = 62.45
+    fieldi = B_func[i]
+    fieldj = B_func[j]
+    int1 = lambda y,x : np.abs(fieldi(y,x))**2 * np.abs(fieldj(y,x))**2
+    top = dblquad(int1,-r,r,lambda x : -r,lambda x: r)[0]
+
+    int2 = lambda y,x : np.abs(fieldi(y,x))**2
+    int3 = lambda y,x : np.abs(fieldj(y,x))**2
+    bottom = dblquad(int2,-r,r,lambda x : -r,lambda x: r)[0]*\
+            dblquad(int3,-r,r,lambda x : -r,lambda x: r)[0]
+    return top/bottom
+
+
+def over2(i,j,k,l,B,B_func):
+    if len(set([i,j,k,l])) == 1:
+        if B[i] == 'lp01':
+            return 1/161
+        else:
+            return 1/170
+    r = 62.45
+    fieldi = B_func[i]
+    fieldj = B_func[j]
+    fieldk = B_func[k]
+    fieldl = B_func[l]
+
+    int1 = lambda y,x : fieldi(y,x) * fieldj(y,x) *fieldk(y,x) * fieldl(y,x)
+    top = dblquad(int1,-r,r,lambda x : -r,lambda x: r)[0]
+    bottom = 1
+    for bot in B_func:
+        int2 = lambda y,x : np.abs(bot(y,x))**2
+        bottom *= dblquad(int2,-r,r,lambda x : -r,lambda x: r)[0]
+    bottom **=0.5
+    return top/bottom
+
+
+def inv_group_disp(i,x,B,mat_lp,lamda_c):
+    x = 2*pi*c/x
+    x= x-lamda_c
+    if B[i] == 'lp01':
+       coeff = mat_lp['Lp01']
+       DD = coeff[0][1]
+    else:
+      coeff = mat_lp['Lp11']
+      DD = coeff[0][1]
+    return coeff[0][0] +coeff[0][1]*x+coeff[0][2]*x**2, -lamda_c**2*DD/(2*pi*c)
+
+
+def Dk_func(omega,lamda_c,B,zeroing,mat_lp):
+  if zeroing == 4:
+    return 0
+  else:
+      omega_c = c*2*pi/lamda_c
+      b = np.zeros(4)
+      for i in range(4):
+        inv_group, dispers = inv_group_disp(i,omega[i],B,mat_lp,lamda_c)
+        inv_group *= -2*pi*c/omega[i]**2
+        b[i] = inv_group *(omega[i] - omega_c) + 0.5*dispers *(omega[i] - omega_c)**2
+      Dk = b[2]+b[3]-b[0]-b[1]
+      return Dk
+
+
+############### The constants
+n2 = 1.1225783990826979e-20 # nonlinear coefficient
+lamda_c = 1.5508e-6
+###############
+
+
+
+num_steps = 10 # initial number of steps for the integrator
+d = 1e3 # propagation distance
+
+zmin =0 #starting point of the fibre
+zmax = d #end point of interest
+
+############## What beam is in what mode
+B = []
+B.append('lp01') #pump1
+B.append('lp01') #pump2
+B.append('lp01') #sigal
+B.append('lp01') #Idler
+B_func = [0, 0, 0 ,0]
+for i,b in enumerate(B):
+    if b == 'lp01':
+        B_func[i] = field0
+    else:
+        B_func[i] = field1
+##############################
+overlap1 = np.zeros([4,4])
+
+mat_lp = loadmat('coeffs.mat')
+
+w_vec = np.loadtxt('data/widths.dat')
+w_vec *=1e6
+for i in range(4):
+    for j in range(4):
+        overlap1[i,j] = over1(i,j,B,B_func)
+i_vec = np.array([[1,2,3],[0,2,3],[3,0,1],[2,0,1]])
+
+overlap2 = np.zeros(4)
+zeroing = 0
+for i in range(4):
+    ii = i_vec[i,:]
+    overlap2[i] = over2(i,ii[0],ii[1],ii[2],B,B_func)
+    if overlap2[i] == 0:
+        zeroing +=1
+
+
+
+overlap1 *=1e12
+overlap2 *=1e12
+zero = np.where(overlap2==0)
+print('calculated the overlaps going for the ode')
+zero = np.asanyarray(zero)
+
+z = np.linspace(zmin,zmax,num_steps) #linearly spaced propagation vector
+dz = 1 * (z[1] - z[0])
+num_steps = (zmax - zmin) / dz +1
+a= 0
+
+
+
+
+
+mat  = loadmat('data/LP11_FWM_data.mat')
+lams_vec_exp= mat['lam_vals']
+D = mat['D']
+del mat
+lamp = np.zeros(len(D[0,:])); lams = np.zeros(len(D[0,:])); lami = np.zeros(len(D[0,:]))
+
+D_p = np.zeros(len(D[0,:])); D_s = np.zeros(len(D[0,:])); D_i = np.zeros(len(D[0,:]))
+for i in range(len(D[0,:])):
+    _max, _min = peakdetect(D[:,i], lams_vec_exp[:,i], 50)
+    max_ = np.asanyarray(_max)
+    max_ = max_[np.argsort(max_[:,1])]
+    lamp[i], D_p[i] = max_[-1::,0][0],max_[-1::,1][0]
+    lami[i], D_i[i] = max_[-3::3,0][0],max_[-3::3,1][0]
+    lams[i], D_s[i] = max_[-2::2,0][0],max_[-2::2,1][0]
+
+D_p = D_p[0:-3:]
+D_s = D_s[0:-3:]
+D_i = D_i[0:-3:]
+
+lamp = lamp[0:-3:]
+lams = lams[0:-3:]
+lami = lami[0:-3:]
+
+P_vec = np.arange(22.7,23.7,2)
+P_vec +=10
+P_vec = dbm_to_w(P_vec)
+
+P_signal_vec = dbm_to_w(D_s) - dbm_to_w(D_i)
+
+
+
+lamp = np.copy(lamp)*1e-9
+lams = np.copy(lams)*1e-9
+lami = np.copy(lami)*1e-9
+
+AB_measured = np.zeros([4,len(P_vec),len(lams)])
+AB_measured[0,0,:] = D_p
+AB_measured[1,0,:] = D_p
+AB_measured[2,0,:] = D_s
+AB_measured[3,0,:] = D_i
+
+
+def FWM(n2,P_vec,dz,num_steps,a):
+    AB_final = np.zeros([4,len(P_vec),len(lams)],dtype='complex')
+    for i,Po in enumerate(P_vec):
+        P = Po
+        for j,lams_ in enumerate(lams):
+            P_signal = P_signal_vec[j]
+
+            AB0 = np.array([P,P, P_signal, 0], dtype='complex')
+            AB0[:] = AB0[:]**0.5
+
+            lami_ = lami[j]
+            lamp_ = lamp[j]
+            lam_vec = np.array([lamp_,lamp_,lams_,lami_])
+
+            omega = 2*pi*c/(lam_vec[:])
+
+            omega[3] = 2*omega[0]-omega[2]
+            lami[j] = 2*pi*c/omega[3]
+
+            Dk = Dk_func(omega,lamda_c,B,zeroing,mat_lp)
+
+
+            int_method = 'dop853'
+            AB_final[:,i,j],outcome = \
+                            integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,a,n2)
+            if outcome==False:
+                print('first try failed, trying adaptive steping...')
+                exits = 0
+                int_method = 'dop853'
+                while exits<=55:
+                    AB_final[:,i,j],outcome = \
+                                   integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,a,n2)
+                    exits +=1
+                    dz *= 0.5
+                    print 'failed, trying step size:', dz,'...'
+                if outcome == False:
+                    sys.exit('All integrations failed')
+    return AB_final
+
+
+AB_final = FWM(n2,P_vec,dz,num_steps,a)
+
+
+
+fig = plt.figure()
+for i, pp in enumerate(w_to_dbm(P_vec)):
+    D_p_sim = np.abs(AB_final[0,i,:])**2
+    plt.plot(lams,w_to_dbm(D_p_sim),'x',label='pp')
+plt.xlabel(r'$\lambda (m)$')
+plt.title('Pump')
+plt.ylabel(r'$P(dbm)$')
+plt.legend(loc=(1.01,0.805))
+plt.savefig('Pump.png',bbox_inches='tight')
+plt.close(fig)
+
+
+
+fig = plt.figure()
+for i, pp in enumerate(w_to_dbm(P_vec)):
+    D_s_sim = np.abs(AB_final[2,i,:])**2
+    plt.plot(lams,w_to_dbm(D_s_sim),'x',label='diff')
+plt.xlabel(r'$\lambda (m)$')
+plt.title('Signal')
+plt.ylabel(r'$P(dbm)$')
+plt.legend(loc=(1.01,0.805))
+plt.savefig('Signal.png',bbox_inches='tight')
+plt.close(fig)
+
+
+fig = plt.figure()
+for i, pp in enumerate(w_to_dbm(P_vec)):
+    D_i_sim = np.abs(AB_final[3,i,:])**2
+    plt.plot(lami,w_to_dbm(D_i_sim),'x',label='sim')
+plt.xlabel(r'$\lambda (m)$')
+plt.title('Idler')
+plt.ylabel(r'$P(dbm)$')
+plt.legend(loc=(1.01,0.805))
+plt.savefig('Idler.png',bbox_inches='tight')
+plt.close(fig)
+
+
+
+
+DeltaE = P_signal_vec + 2*P_vec*np.ones(len(P_signal_vec)) - \
+        D_i_sim - 2*D_p_sim - D_s_sim
+print 'Infinity norm of the energy difference', np.linalg.norm(DeltaE,np.inf)
--- FWM_agrawal_n2_finder.py
+++ FWM_agrawal_n2_finder.py
@@ -10,7 +10,7 @@ import matplotlib.pylab as plt
 from scipy.constants import pi,c
 from scipy.integrate import ode
 from scipy.io import loadmat
-from scipy.optimize import minimize, fsolve, fmin, fmin_slsqp
+from scipy.optimize import fsolve
 import sys
 from numpy.linalg import norm
 from peakdetect import peakdetect
@@ -24,7 +24,7 @@ def w_to_dbm(W):
     return 10.*np.log10(W)+30
 
 def integrand(i, A, zz,omega,Dk,n2,over1):
-    """Calculates the integrand of the 3 coupled ode's according to 
+    """Calculates the integrand of the 3 coupled ode's according to
         Fiber-Based Optical Parametric Amplifiers
         and Their Applications
         """
@@ -41,8 +41,8 @@ def integrand(i, A, zz,omega,Dk,n2,over1):
 
     elif i ==2:
         temp = A[2]*(np.abs(A[2])**2 + 2*np.abs(A[1])**2 + 2*np.abs(A[0])**2) + \
-                np.conj(A[1])*(A[0]**2)*np.exp(-1j*Dk*zz) 
-        
+                np.conj(A[1])*(A[0]**2)*np.exp(-1j*Dk*zz)
+
 
     gama = over1()*n2*omega/c
     return 1j*gama *temp
@@ -52,13 +52,13 @@ def system(zz,AB,omega,Dk,n2,over1):
     """
         The system of differentail equations to be solved. Converts the floats
         to coplex and is used by scipy.integrate.ode
-    """    
-    
+    """
+
     try: #This is put here because ode & odeint take their x,y values the other way round
         temp = AB[0]
     except:
         temp = AB
-        AB = np.copy(zz)        
+        AB = np.copy(zz)
         zz = np.copy(temp)
         del temp
         pass
@@ -67,18 +67,18 @@ def system(zz,AB,omega,Dk,n2,over1):
     A1 = AB[2] + AB[3]*1j
     A2 = AB[4] + AB[5]*1j
     A = np.array([A0, A1, A2])
-    
+
     for i in range(3):
         dABdz[i] = integrand(i,A,zz,omega[i],Dk,n2,over1)
-    return dABdz.view(np.float64) 
+    return dABdz.view(np.float64)
 
 
 def integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,n2,over1):
     """
     Performs the integration using the scipy.integrate.ode class. the int_method
-    determines the method to be used.    
-    """    
-    
+    determines the method to be used.
+    """
+
     r = ode(system).set_integrator(int_method)
     r.f_params =(omega,Dk,n2,over1,)
     r.set_initial_value(AB0.view(np.float64),np.float64(zmin))
@@ -88,7 +88,7 @@ def integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,n2,over1):
             r.integrate(r.t+dz)
             count+=1
     if r.successful():
-        AB_final[:,i,j] = r.y.view(np.complex128)      
+        AB_final[:,i,j] = r.y.view(np.complex128)
     return AB_final[:,i,j], r.successful()
 
 def FWM(n2,P_vec,dz,num_steps,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1):
@@ -99,10 +99,10 @@ def FWM(n2,P_vec,dz,num_steps,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1)
     """
     AB_final = np.zeros([3,len(P_vec),len(lams)],dtype='complex')
     for i,Po in enumerate(P_vec):
-        P = Po  
+        P = Po
         for j,lams_ in enumerate(lams):
             P_signal = P_signal_vec[j]
-            
+
             AB0 = np.array([P,P_signal, 0], dtype='complex')
             AB0[:] = AB0[:]**0.5
 
@@ -114,19 +114,22 @@ def FWM(n2,P_vec,dz,num_steps,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1)
 
             omega[2] = 2*omega[0]-omega[1]
             lami[j] = 2*pi*c/omega[2]
-            
+
             Dk = 0.5*beta2*(omega[1]**2+omega[2]**2-2*omega[0]**2)
-            int_method = 'dop853' #DP method of integration 
+            #print (omega[1]**2+omega[2]**2-2*omega[0]**2)
+            #print '...'
+            print (omega[1]**2+omega[2]**2-2*omega[0]**2)
+            int_method = 'dop853' #DP method of integration
             AB_final[:,i,j],outcome = \
                             integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,n2,over1)
             if not(outcome):
-                print('first try failed, trying adaptive steping...')            
+                print('first try failed, trying adaptive steping...')
                 exits = 0
-                int_method = 'dop853'  
+                int_method = 'dop853'
                 while exits<=55:
                     AB_final[:,i,j],outcome = \
-                                   integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,n2,over1) 
-                    exits +=1           
+                                   integrator(int_method,AB0,zmin,zmax,dz,AB_final,i,j,omega,Dk,n2,over1)
+                    exits +=1
                     dz *= 0.5
                     print 'failed, trying step size:', dz,'...'
             if not(outcome):
@@ -134,9 +137,9 @@ def FWM(n2,P_vec,dz,num_steps,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1)
     return AB_final
 
 def searcher(n2,P_vec,dz,num_steps,AB_measured,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1):
-    
+
     AB_final = FWM(n2,P_vec,dz,num_steps,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1)
-   
+
     return  norm(np.abs(AB_final[2])**2 - dbm_to_w(AB_measured[2]),2)
 
 
@@ -146,13 +149,13 @@ def inputs(filename):
     simmulated to find n2.
     """
     mat  = loadmat('data/LP11_FWM_data.mat')
-    mat = loadmat(filename)
+    #mat = loadmat(filename)
     lams_vec_exp= mat['lam_vals']
     D = mat['D']
     del mat
-    lamp = np.zeros(len(D[0,:])); lams = np.zeros(len(D[0,:])); lami = np.zeros(len(D[0,:])) 
-    
-    D_p = np.zeros(len(D[0,:])); D_s = np.zeros(len(D[0,:])); D_i = np.zeros(len(D[0,:])) 
+    lamp = np.zeros(len(D[0,:])); lams = np.zeros(len(D[0,:])); lami = np.zeros(len(D[0,:]))
+
+    D_p = np.zeros(len(D[0,:])); D_s = np.zeros(len(D[0,:])); D_i = np.zeros(len(D[0,:]))
     for i in range(len(D[0,:])):
         _max, _min = peakdetect(D[:,i], lams_vec_exp[:,i], 50)
         max_ = np.asanyarray(_max)
@@ -160,25 +163,25 @@ def inputs(filename):
         lamp[i], D_p[i] = max_[-1::,0][0],max_[-1::,1][0]
         lami[i], D_i[i] = max_[-3::3,0][0],max_[-3::3,1][0]
         lams[i], D_s[i] = max_[-2::2,0][0],max_[-2::2,1][0]
-    
+
     D_p = D_p[0:-3:]
     D_s = D_s[0:-3:]
     D_i = D_i[0:-3:]
-    
+
     lamp = lamp[0:-3:]
     lams = lams[0:-3:]
     lami = lami[0:-3:]
     P_vec = np.arange(22.7,23.7,2)
     P_vec +=10
     P_vec = dbm_to_w(P_vec)
-    
+
     P_signal_vec = dbm_to_w(D_s) - dbm_to_w(D_i)
-    
-    
+
+
     lamp = np.copy(lamp)*1e-9
     lams = np.copy(lams)*1e-9
     lami = np.copy(lami)*1e-9
-    
+
     AB_measured = np.zeros([3,len(P_vec),len(lams)])
     AB_measured[0,0,:] = D_p
     AB_measured[1,0,:] = D_s
@@ -193,7 +196,7 @@ def plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured):
     D_p_sim,  D_s_sim,  D_i_sim =  unpack(AB_final)
 
     D_p,  D_s,  D_i =  AB_measured[0,0,:] ,AB_measured[1,0,:], AB_measured[2,0,:]
-    D_p += 10    
+    D_p += 10
     fig = plt.figure()
     plt.plot(n2_foundp,norms)
     plt.xlabel(r'$n_2$')
@@ -201,8 +204,8 @@ def plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured):
     plt.title('$n_2$:'+str(n2_found[0][0])+', err:'+str(n2_found[1]['fvec']))
     plt.savefig('n2.png')
     plt.close(fig)
-    
-    
+
+
     fig = plt.figure()
     plt.plot(lams,w_to_dbm(D_p_sim),'x',label='sim')
     plt.plot(lams,D_p,'o',label = 'meas')
@@ -213,9 +216,9 @@ def plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured):
     plt.legend(loc=(1.01,0.805))
     plt.savefig('Pump.png',bbox_inches='tight')
     plt.close(fig)
-    
-    
-    
+
+
+
     fig = plt.figure()
     plt.plot(lams,w_to_dbm(D_s_sim),'x',label='sim' )
     plt.plot(lams,np.abs(D_s - w_to_dbm(D_s_sim)),label='diff')
@@ -226,10 +229,10 @@ def plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured):
     plt.legend(loc=(1.01,0.805))
     plt.savefig('Signal.png',bbox_inches='tight')
     plt.close(fig)
-    
-    
+
+
     fig = plt.figure()
-    
+
     plt.plot(lami,w_to_dbm(D_i_sim),'x',label='sim')
     plt.plot(lami,D_i,'o',label = 'meas')
     plt.plot(lami,np.abs(D_i - w_to_dbm(D_i_sim)),label='diff')
@@ -239,7 +242,7 @@ def plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured):
     plt.legend(loc=(1.01,0.805))
     plt.savefig('Idler.png',bbox_inches='tight')
     plt.close(fig)
-    return 
+    return
 def energy_conservation(P_signal_vec,P_vec,AB_final):
     D_p_sim,  D_s_sim,  D_i_sim = unpack(AB_final)
     DeltaE = P_signal_vec + P_vec*np.ones(len(P_signal_vec)) - \
@@ -251,31 +254,31 @@ def energy_conservation(P_signal_vec,P_vec,AB_final):
 
 def unpack(AB_final):
     """
-    unpacks the powers for the pump signal and idler. 
+    unpacks the powers for the pump signal and idler.
     """
     D_p_sim = np.abs(AB_final[0,0,:])**2
     D_s_sim = np.abs(AB_final[1,0,:])**2
-    D_i_sim = np.abs(AB_final[2,0,:])**2    
+    D_i_sim = np.abs(AB_final[2,0,:])**2
     return  D_p_sim,  D_s_sim,  D_i_sim
 
-          
+
 
 
 
 
 
 def main():
-    
-    
+
+
     n2 = 3e-20 # nonlinear coefficient (guess)
     num_steps = 10 # initial number of steps for the integrator, if fails the stepis refined
     d = 1e3 # Propagation distance of the waves
     mode='lp11' # mode that is simmulated, only LP01 LP11 with no cross mode interaction(only one mode launched)
     lamda_c = 1.5508e-6 #The central freequency at which the dispersion is calculated
     zmin =0 #starting point of the fibre
-    zmax = d #end point of interest        
+    zmax = d #end point of interest
     z = np.linspace(zmin,zmax,num_steps) #linearly spaced propagation vector
-    
+
     dz = 1 * (z[1] - z[0])
     del z
     num_steps = (zmax - zmin) / dz +1
@@ -291,7 +294,7 @@ def main():
         DD = 22*1e-6
     AB_measured,P_signal_vec,P_vec,lamp,lams,lami = inputs(filename)
     print AB_measured
-    beta2 = -lamda_c**2*DD/(2*pi*c) 
+    beta2 = -lamda_c**2*DD/(2*pi*c)
     #print n2_found
     #sys.exit()
     #bound=((1.5e-20,2e-20),)
@@ -301,7 +304,7 @@ def main():
     #print searcher(n2_found,P_vec,dz,num_steps,AB_measured)
     #n2_found = fmin(searcher,n2,args=(P_vec,dz,num_steps,AB_measured),full_output=1)
     #n2_found = fmin_slsqp(searcher,n2,bounds = bound,args=(P_vec,dz,num_steps,AB_measured))
-    
+
     #n2_found = minimize(searcher,n2,args=(P_vec,dz,num_steps,AB_measured))
     #print searcher(n2_found,P_vec,dz,num_steps,AB_measured)
 
@@ -309,14 +312,14 @@ def main():
 
     n2_foundp = np.arange(1e-22,3e-20,1e-21)
     norms = np.zeros(len(n2_foundp))
-    
+
     for i in range(len(norms)):
         norms[i] = searcher(n2_foundp[i],P_vec,dz,num_steps,AB_measured,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1)
     #print 'found' , n2_found[0]
     #print 'idler norm:', searcher(n2_found[0],P_vec,dz,num_steps,AB_measured)
 
-    
-    
+
+
 
     n2_found =fsolve(searcher,n2,args=(P_vec,dz,num_steps,AB_measured,zmin,zmax,lamp,lams,lami,beta2,P_signal_vec,over1),full_output=1,xtol=1e-33)
     print 'Found: n2 = ', n2_found[0][0]
@@ -326,6 +329,6 @@ def main():
     plotter(norms,n2_foundp,n2_found, lams,lami,AB_final, AB_measured)
     energy_conservation(P_signal_vec,P_vec,AB_final)
     return 0
-    
+
 if __name__ == '__main__':
     main()
